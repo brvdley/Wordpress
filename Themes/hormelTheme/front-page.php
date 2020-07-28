305f66af62e953fb647e6e0fdf4dc6fed2baf62b
brvdley <bradleyowens126@gmail.com>
2020-07-28 16:13:08 -0500

    Hormel Test Theme

---
+++ Themes/hormelTheme/front-page.php
@@ -0,0 +1,124 @@
+<?php get_header(); ?>
+<div class="page">
+  <nav>
+    <div class="navWr">
+      <a href="#">PRODUCTS</a>
+      <a href="#">FAQ</a>
+      <div class="logo">
+        <div class="nImg">
+          <img src="images/hormel.png" alt="Hormel Logo" id="logoImg">
+        </div>
+        <div class="nText">
+          <h3 id="logoText">INGREDIENT SOLUTIONS</h3>
+        </div>
+      </div>
+      <a href="#">ABOUT</a>
+      <a href="#">CONTACT</a>
+    </div>
+  </nav>
+  <header>
+    <div class="headerWr">
+      <div class="hText">
+        <h2><span id="hFancy">High-Quality</span> Protein Products</h2>
+        <p class="column">Quality recipes start with quality ingredients. Find the widest variety and assortment of protein solutions for your final
+        applications, from foundational products to fully cooked meats.</p>
+      </div>
+      <div class="hButton">
+        <a href="#" id="btnTemp">EXPLORE CATALOG</a>
+      </div>
+    </div>
+  </header>
+  <section id="middle">
+    <div class="mText">
+      <h2>The Hormel <span id="hFancy">Difference</span></h2>
+      <p class="column2">Dedication to providing customers with superior ingredients has inspired Hormel since 1891. The Ingredient Solutions branch focuses on the highest quality protein-based ingredients.
+      Our team works each day to give prepared food manufacturers the competitive edge needed to bring their formulations to life and expand their own portfolio and bring their applications to life.</p>
+    </div>
+    <div class="mBtn">
+      <div class="hButton">
+        <a href="#" id="btnTemp">GET TO KNOW US</a>
+      </div>
+    </div>
+  </section>
+  <section id="family">
+    <div id="round">
+    </div>
+  </section>
+  <section id="items">
+    <div class="topSection">
+      <div class="iText1">
+        <h2 id="words1"><span id="hGreen">Superior <img src="images/pathUp.png" alt="Line arced in an upwards direction."></span> Food Ingredients</h2>
+        <p class="column4">Food manufacturers partner with the Hormel team to find inspiration for new, unique formulations that end up on dinner tables across America. These become part
+          of recipes that get passed down from generation to generation, nourishing families and putting smiles on faces.</p>
+      </div>
+      <div class="iPic1">
+        <img src="images/group.png" alt="Scoops containing multiple types of seasoning.">
+      </div>
+    </div>
+    <div class="bottomSection">
+      <div class="iPic2">
+        <img src="images/salad.png" alt="Salad in a bowl on a table.">
+      </div>
+      <div class="iText2">
+        <h2 id="words2"><span id="hGreen">Industry-Leading <img src="images/pathDown.png" alt="Line arced in a downwards direction."></span> Experts</h2>
+        <p class="column3">Over a century of combined experience in the food industry helps us rise to meet any solution needed. With a customer-minded approach, we collaborate with you to
+          understand what a consumer wants in order to find the best solution to introduce the final formulation to market.</p>
+      </div>
+    </div>
+  </section>
+  <section id="soup">
+    <div class="sText">
+      <h2 id="words2">Work with our <span id="hGreen"> &nbsp;Solutions-Driven<img src="images/pathDown.png" alt="Line arced in a downwards direction."></span> Team</h2>
+      <p class="column2">Take advantage of a full network of resources to inspire, guide, and provide peace of mind while working with the Hormel team. Our problem-solving experts are on hand to answer questions and find
+        the right solutions for any unique need. Anything is possible when you find the right ingredient partner.</p>
+    </div>
+    <div class="sBtn">
+      <div class="hButton">
+        <a href="#" id="btnTemp">LET'S TALK</a>
+      </div>
+    </div>
+    <div class="pattern">
+      <h2>Bring your ideas <span id="hFancy">to Life</span></h2>
+      <p class="column" style="font-size: 14px;">It’s difficult to create a quality formulation without sampling the ingredients yourself.</p>
+    </div>
+    <div class="pBtn">
+      <div class="hButton">
+        <a href="#" id="btnTemp">REQUEST A SAMPLE</a>
+      </div>
+    </div>
+  </section>
+  <section id="bottom1">
+    <div class="left">
+      <div class="social">
+        <div class="icon"><img src="images/facebook.png" alt="An icon representing the FaceBook company."></div>
+        <div class="icon"><img src="images/twitter.png" alt="An icon representing the Twitter company."></div>
+        <div class="icon"><img src="images/instagram.png" alt="An icon representing the Instagram company."></div>
+      </div>
+      <div class="phone">
+        <img src="images/phone.png" alt="An icon representing a phone ringing.">
+        <p id="pNum">(507) 434-6374</p>
+      </div>
+    </div>
+    <div class="right">
+      <div class="email">
+        <img src="images/email.png" alt="An icon representing an envelope.">
+        <p id="email">IngredientSolutions@Hormel.com</p>
+      </div>
+    </div>
+  </section>
+  <section id="bottom2">
+    <div class="bottomNav">
+      <a href="#">Privacy Policy | </a>
+      <a href="#">Terms Of Use | </a>
+      <a href="#">Sitemap | </a>
+      <a href="#">FAQ | </a>
+      <a href="#">California Privacy Policy | </a>
+      <a href="#">California Collection Notice | </a>
+      <a href="#">Do Not Sell My Info</a>
+    </div>
+    <div class="copyright">
+    <p id="copyright">&copy;<?php echo date("Y");?> Hormel Ingredient Solutions. All rights reserved.</p>
+    </div>
+  </section>
+</div>
+<?php get_footer(); ?>
---
+++ Themes/hormelTheme/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Hormel Theme For WebSpec">
+    <meta name="author" content="Bradley Owens">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700&family=Satisfy&family=Slabo+13px&display=swap" rel="stylesheet">
+    <?php wp_head();?>
+  </head>
+  <body <?php body_class();?>>
